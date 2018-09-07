915719f44b1b3871d2a6f4b2d7537574031f6a5e
Rasmu <Rasmu@10.50.131.181>
2018-09-07 14:22:38 +0200

    Nyt Nyt Nyt

---
+++ Git Test/src/git/test/GitTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.test;
+
+/**
+ *
+ * @author Rasmu
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int dinMor = 0;
+        int dinFar = 1;
+        
+        if (dinMor > dinFar) {
+            System.out.println("knep dig selv");
+        }
+        else {
+            System.out.println("....");
+        }
+    }
+    
+}
